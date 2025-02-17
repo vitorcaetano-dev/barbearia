0488059211786756819a966fe84e948891c4b75b
vitor caetano <vitorcaetano505@gmail.com>
2025-02-17 14:41:25 -0300

    m

--- barbearia.js
+++ barbearia.js
@@ -1,3 +1,22 @@
+const btn_marca = document.getElementById("marcar");
+
+btn_marca.addEventListener("click", () => {
+  const contato = document.getElementById("contact");
+  if (contato) {
+    contato.scrollIntoView({ behavior: "smooth" });
+  }
+});
+
+function menu() {
+  const itens = document.getElementById("navbarBasicExample");
+  if (itens.style.display == "block") {
+    itens.style.display = "none";
+  } else {
+    itens.style.display = "block"
+    
+  }
+}
+
 function enviar() {
   const nome = document.getElementById("iname").value;
   const data = document.getElementById("idate").value;
@@ -6,8 +25,7 @@ function enviar() {
   console.log(nome, data, hora);
 
   const numero = "553399075738";
-  const mensagem = 
-  `Olá, meu nome é ${nome}. Gostaria de agendar um corte para o dia ${data} às ${hora}.`
+  const mensagem = `Olá, meu nome é ${nome}. Gostaria de agendar um corte para o dia ${data} às ${hora}.`;
 
   const url = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`;
 
